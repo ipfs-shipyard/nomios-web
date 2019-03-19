47afea18d4bb857c6e214286a3ef67222cffa2be
Pedro Santos <pedromiguelsousasantos@hotmail.com>
2019-03-19 10:47:06 +0000

    chore: repo tooling setup (#1)

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import styles from './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className={ styles.app }>
+                <header className={ styles.appHeader }>
+                    <img src={ logo } className={ styles.appLogo } alt="logo" />
+                    <p>Edit <code>src/App.js</code> and save to reload.</p>
+                    <a className={ styles.appLink }
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
