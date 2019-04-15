95cf573b2a2b02eb5ecec42cced64da0e61c3f48
Gil Domingues <gil.domingues2@hotmail.com>
2019-04-15 15:24:47 +0100

    chore: made some of the requested changes

--- src/shared/components/lock-screen/LockScreen.js
+++ src/shared/components/lock-screen/LockScreen.js
@@ -31,6 +31,21 @@ class LockScreen extends Component {
         loop: false,
     };
 
+    lockscreenContentClassNames = {
+        appear: styles.lockscreenContentAppear,
+        appearActive: styles.lockscreenContentAppearActive,
+        exit: styles.lockscreenContentExit,
+        exitActive: styles.lockscreenContentExitActive,
+        exitDone: styles.lockscreenContentExitDone,
+    };
+
+    passwordDotsClassNames = {
+        enter: styles.passwordDotEnter,
+        enterActive: styles.passwordDotEnterActive,
+        exit: styles.passwordDotExit,
+        exitActive: styles.passwordDotExitActive,
+    };
+
     state = {
         passwordLength: 0,
         feedback: 'none',
@@ -52,7 +67,7 @@ class LockScreen extends Component {
         return (
             <div className={ styles.lockscreen } onClick={ this.handleMouseClick }>
                 <div className={ styles.background } dangerouslySetInnerHTML={ { __html: background } } />
-                <CSSTransition classNames={ styles.lockscreenContent } in={ !unmounting } appear component={ null } timeout={ 1500 }>
+                <CSSTransition classNames={ this.lockscreenContentClassNames } in={ !unmounting } appear component={ null } timeout={ 1500 }>
                     <div className={ styles.lockscreenContent }>
                         <div className={ styles.logo }>
                             <Lottie options={ this.animationOptions } className={ styles.svg } isPaused={ !startLogoAnimation } />
@@ -90,7 +105,7 @@ class LockScreen extends Component {
                     {
                         Array(passwordLength).fill(0)
                         .map((val, i) => (
-                            <CSSTransition classNames={ styles.passwordDot } key={ passwordLength - i } timeout={ 50 }>
+                            <CSSTransition classNames={ this.passwordDotsClassNames } key={ passwordLength - i } timeout={ 50 }>
                                 <div className={ passwordClassName } style={ { animationDelay: `${animationDelay * i}s` } } />
                             </CSSTransition>
                         ))
