8387b4dc14aaf400ddae965fe6f83886ebef1ae4
André Cruz <andremiguelcruz@msn.com>
2019-06-02 16:32:11 +0100

    perf: lazy load node-geocoder library
    Since it's kind of heavy.

--- src/modals/edit-profile/profile-form/location-input/get-location.js
+++ src/modals/edit-profile/profile-form/location-input/get-location.js
@@ -1,25 +1,32 @@
 import pTimeout from 'p-timeout';
 import pify from 'pify';
-import nodeGeocoder from 'node-geocoder';
 
-const GET_GEOLOCATION_TIMEOUT = 10000;
-
-const geocoder = nodeGeocoder({
-    provider: 'openstreetmap',
-    format: 'json',
-    zoom: 18,
-    addressdetails: 1,
-    'accept-language': 'en-US',
-});
+const GET_GEOLOCATION_TIMEOUT = 15000;
 
 const getLocation = async () => {
+    const nodeGeocoder = await pTimeout(
+        import(/* webpackChunkName: "node-geocoder" */ 'node-geocoder'),
+        GET_GEOLOCATION_TIMEOUT
+    );
+
+    const geocoder = nodeGeocoder.default({
+        provider: 'openstreetmap',
+        format: 'json',
+        zoom: 18,
+        addressdetails: 1,
+        'accept-language': 'en-US',
+    });
+
     const position = await new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject);
     });
 
     const { latitude, longitude } = position.coords;
 
-    const response = await pTimeout(pify(geocoder.reverse).call(geocoder, { lat: latitude, lon: longitude }), GET_GEOLOCATION_TIMEOUT);
+    const response = await pTimeout(
+        pify(geocoder.reverse).call(geocoder, { lat: latitude, lon: longitude }),
+        GET_GEOLOCATION_TIMEOUT
+    );
 
     return response.raw.address;
 };
