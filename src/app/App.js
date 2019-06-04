b5696b6aa892b100455e2876b8c1ec49cebc6a6c
André Cruz <andremiguelcruz@msn.com>
2019-06-04 10:03:43 +0100

    fix: make sidebar position fixed (#17)

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { connectIdmWallet } from 'react-idm-wallet';
 import { Transition } from 'react-transition-group';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Shell from './shell';
+import Sidebar from './sidebar';
 import SetupLocker from '../modals/setup-locker';
 import Home from '../pages/home';
 import Identity from '../pages/identity';
@@ -20,19 +21,22 @@ class App extends Component {
     }
 
     render() {
+        const { className } = this.props;
         const { setupLockerOpen } = this.state;
 
         return (
-            <div className={ styles.app }>
+            <div className={ classNames(styles.app, className) }>
                 <Router>
+                    <Sidebar className={ styles.sidebar } />
+
+                    <main className={ styles.page }>
+                        <Route path="/" exact component={ Home } />
+                        <Route path="/identity/:id" component={ Identity } />
+                    </main>
+
                     <Transition in={ setupLockerOpen } timeout={ 2000 } mountOnEnter unmountOnExit>
                         <SetupLocker open={ setupLockerOpen } onComplete={ this.handleSetupLockerComplete } />
                     </Transition>
-
-                    <Shell>
-                        <Route path="/" exact component={ Home } />
-                        <Route path="/identity/:id" component={ Identity } />
-                    </Shell>
                 </Router>
             </div>
         );
@@ -45,6 +49,7 @@ class App extends Component {
 
 App.propTypes = {
     pristine: PropTypes.bool,
+    className: PropTypes.string,
 };
 
 export default connectIdmWallet((idmWallet) => () => ({
