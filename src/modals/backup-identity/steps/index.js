345477ee05b25c170b352d536e145ffb95c27a4b
Pedro Santos <pedro.santos@moxy.studio>
2019-06-13 10:49:10 +0100

    chore: add write-words step

---
+++ src/modals/backup-identity/steps/index.js
@@ -0,0 +1,9 @@
+import Passphrase from './passphrase';
+import PaperKey from './paper-key';
+import WriteWords from './write-words';
+
+export {
+    Passphrase,
+    PaperKey,
+    WriteWords,
+};
---
+++ src/modals/backup-identity/steps/write-words/mnemonic-box/MnemonicBox.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import memoizeOne from 'memoize-one';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { TextButton, EditIcon } from '@nomios/web-uikit';
+import styles from './MnemonicBox.css';
+
+class MnemonicBox extends Component {
+    state = { copied: false };
+    stringifyMnemonic = memoizeOne((mnemonic) => mnemonic.join(' '));
+
+    render() {
+        const { mnemonic } = this.props;
+        const { copied } = this.state;
+
+        return (
+            <div className={ styles.box }>
+                <div className={ styles.top }>
+                    { this.renderWords(mnemonic) }
+                </div>
+                <div className={ styles.bottom }>
+                    <CopyToClipboard text={ this.stringifyMnemonic(mnemonic) } onCopy={ this.handleOnCopy }>
+                        <TextButton
+                            variant="small"
+                            iconPosition="left"
+                            icon={ <EditIcon /> }>
+                            { copied ? 'Copied!' : 'Copy to clipboard' }
+                        </TextButton>
+                    </CopyToClipboard>
+                </div>
+            </div>
+        );
+    }
+
+    renderWords = memoizeOne((mnemonic) => mnemonic.map((word, index) => {
+        const wordNumber = `0${index + 1}`.slice(-2);
+
+        return (
+            <div key={ index } className={ styles.wordContainer }>
+                <span className={ styles.number }>{ `#${wordNumber}` }</span>
+                <span>{ word }</span>
+            </div>
+        );
+    }));
+
+    handleOnCopy = () => {
+        this.setState({ copied: true });
+    };
+}
+
+MnemonicBox.propTypes = {
+    mnemonic: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default MnemonicBox;
