3def89f2eb9417f471ab7c76ec1ccff04e13a8e6
André Cruz <andremiguelcruz@msn.com>
2019-06-29 00:33:02 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.72e4109a01d6f8b82f95f517d1cf9166.js"
+  "/precache-manifest.fbd24aab133c67b79b58ea21355bbce2.js"
 );
 
 self.addEventListener('message', (event) => {
