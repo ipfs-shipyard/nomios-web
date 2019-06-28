9757c06a2c1cd600e5cb6e10f1a4a0414039cfd2
Pedro Santos <pedro.santos@moxy.studio>
2019-06-28 13:13:30 +0200

    chore: change import identity success button copy

--- src/modals/new-identity-flow/NewIdentityFlow.js
+++ src/modals/new-identity-flow/NewIdentityFlow.js
@@ -166,7 +166,7 @@ class NewIdentityFlow extends Component {
     renderImportFeedbackSuccessActions() {
         return (
             <Fragment>
-                <Button variant="primary" onClick={ this.props.onRequestClose }>Go to Homepage</Button>
+                <Button variant="primary" onClick={ this.props.onRequestClose }>Go to Identity page</Button>
             </Fragment>
         );
     }
